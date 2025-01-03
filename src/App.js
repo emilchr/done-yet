c5fb698ad14adc24a811b33a778a55e09c403049
emilchr <98517843+emilchr@users.noreply.github.com01>
2025-01-03 16:17:03 +0100

    Styled the concealer.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header>
         <img src={logo} alt='Done Yet Logo' />
+        <div className='toast'>Task created!</div>
       </header>
       <div className="todo-list">
         <div className='todo-uncompleted'>
@@ -24,8 +25,10 @@ function App() {
           <hr />
           <div className="todo-completed">
             <div className='todo'><span>Todo 4</span><input type='checkbox' checked /><label for='checkbox'></label></div>
-            <div className='todo'><span>Todo 4</span><input type='checkbox' checked /><label for='checkbox'></label></div>
-            <div className='todo'><span>Todo 4</span><input type='checkbox' checked /><label for='checkbox'></label></div>
+            <div className='todo'><span>Todo Test</span><input type='checkbox' checked /><label for='checkbox'></label></div>
+            <div className='todo'><span>Todo Test</span><input type='checkbox' checked /><label for='checkbox'></label></div>
+            <div className='todo'><span>Todo Test</span><input type='checkbox' checked /><label for='checkbox'></label></div>
+            <div className='todo'><span>Todo with too much text for it's own good. Lorem ipsum.</span><input type='checkbox' checked /><label for='checkbox'></label></div>
           </div>
         </div>
       </div>
