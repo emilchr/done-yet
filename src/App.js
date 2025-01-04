09a5ec3363f17db4d96e61f62c62997cdd31318c
emilchr <98517843+emilchr@users.noreply.github.com01>
2025-01-04 19:32:53 +0100

    Optimized for larger screen sizes.

--- src/App.js
+++ src/App.js
@@ -6,35 +6,113 @@ function App() {
   return (
     <div className="App">
       <header>
-        <img src={logo} alt='Done Yet Logo' />
-        <div className='toast'>Task created!</div>
+        <img src={logo} alt="Done Yet Logo" />
+        {/* <div className="toast">Task created!</div> */}
       </header>
       <div className="todo-list">
-        <div className='todo-uncompleted'>
-          <div className='todo'><span>Todo 1</span><input type='checkbox' /><label for='checkbox'></label></div>
-          <div className='todo'><span>Todo 1</span><input type='checkbox' /><label for='checkbox'></label></div>
-          <div className='todo'><span>Todo 1</span><input type='checkbox' /><label for='checkbox'></label></div>
-          <div className='todo'><span>Todo 1</span><input type='checkbox' /><label for='checkbox'></label></div>
-          <div className='todo'><span>Todo 1</span><input type='checkbox' /><label for='checkbox'></label></div>
-          <div className='todo'><span>Todo 1</span><input type='checkbox' /><label for='checkbox'></label></div>
-          <div className='todo'><span>Todo 1</span><input type='checkbox' /><label for='checkbox'></label></div>
-          <div className='todo'><span>Todo 1</span><input type='checkbox' /><label for='checkbox'></label></div>
-          <div className='todo'><span>Todo 1</span><input type='checkbox' /><label for='checkbox'></label></div>
+        <div className="todo-uncompleted">
+          <div className="todo">
+            <span>Todo 1</span>
+            <input type="checkbox" />
+            <label for="checkbox"></label>
+          </div>
+          <div className="todo">
+            <span>Todo 1</span>
+            <input type="checkbox" />
+            <label for="checkbox"></label>
+          </div>
+          <div className="todo">
+            <span>Todo 1</span>
+            <input type="checkbox" />
+            <label for="checkbox"></label>
+          </div>
+          <div className="todo">
+            <span>Todo 1</span>
+            <input type="checkbox" />
+            <label for="checkbox"></label>
+          </div>
+          <div className="todo">
+            <span>Todo 1</span>
+            <input type="checkbox" />
+            <label for="checkbox"></label>
+          </div>
+          <div className="todo">
+            <span>Todo 1</span>
+            <input type="checkbox" />
+            <label for="checkbox"></label>
+          </div>
+          <div className="todo">
+            <span>Todo 1</span>
+            <input type="checkbox" />
+            <label for="checkbox"></label>
+          </div>
+          <div className="todo">
+            <span>Todo 1</span>
+            <input type="checkbox" />
+            <label for="checkbox"></label>
+          </div>
+          <div className="todo">
+            <span>Todo 1</span>
+            <input type="checkbox" />
+            <label for="checkbox"></label>
+          </div>
+          <div className="todo">
+            <span>Todo 1</span>
+            <input type="checkbox" />
+            <label for="checkbox"></label>
+          </div>
+          <div className="todo">
+            <span>Todo 1</span>
+            <input type="checkbox" />
+            <label for="checkbox"></label>
+          </div>
+          <div className="todo">
+            <span>Todo 1</span>
+            <input type="checkbox" />
+            <label for="checkbox"></label>
+          </div>
+          <div className="todo">
+            <span>Todo 1</span>
+            <input type="checkbox" />
+            <label for="checkbox"></label>
+          </div>
         </div>
-        <div className='concealer'>
+        <div className="show">
           <hr />
           <div className="todo-completed">
-            <div className='todo'><span>Todo 4</span><input type='checkbox' checked /><label for='checkbox'></label></div>
-            <div className='todo'><span>Todo Test</span><input type='checkbox' checked /><label for='checkbox'></label></div>
-            <div className='todo'><span>Todo Test</span><input type='checkbox' checked /><label for='checkbox'></label></div>
-            <div className='todo'><span>Todo Test</span><input type='checkbox' checked /><label for='checkbox'></label></div>
-            <div className='todo'><span>Todo with too much text for it's own good. Lorem ipsum.</span><input type='checkbox' checked /><label for='checkbox'></label></div>
+            <div className="todo">
+              <span>Todo 4</span>
+              <input type="checkbox" checked />
+              <label for="checkbox"></label>
+            </div>
+            <div className="todo">
+              <span>Todo Test</span>
+              <input type="checkbox" checked />
+              <label for="checkbox"></label>
+            </div>
+            <div className="todo">
+              <span>Todo Test</span>
+              <input type="checkbox" checked />
+              <label for="checkbox"></label>
+            </div>
+            <div className="todo">
+              <span>Todo Test</span>
+              <input type="checkbox" checked />
+              <label for="checkbox"></label>
+            </div>
+            <div className="todo">
+              <span>
+                Todo with too much text for it's own good. Lorem ipsum.
+              </span>
+              <input type="checkbox" checked />
+              <label for="checkbox"></label>
+            </div>
           </div>
         </div>
       </div>
-      <div className='user-input'>
-        <input type='text' name='user-input'></input>
-        <button className='add-button'></button>
+      <div className="user-input">
+        <input type="text" name="user-input"></input>
+        <button className="add-button"></button>
       </div>
     </div>
   );
